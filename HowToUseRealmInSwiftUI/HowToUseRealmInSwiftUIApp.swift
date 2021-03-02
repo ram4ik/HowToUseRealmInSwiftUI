0aea8845bada696c894250e47f16d46858d948dc
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-03-02 20:19:17 +0200

    Initial Commit

---
+++ HowToUseRealmInSwiftUI/HowToUseRealmInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseRealmInSwiftUIApp.swift
+//  HowToUseRealmInSwiftUI
+//
+//  Created by Ramill Ibragimov on 02.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseRealmInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
